03eea4d8c508f2e06e856eeb62465f1b58bea0f1
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-04-06 16:58:04 -0700

    Added requirements and config settings

---
+++ config/settings.py
@@ -0,0 +1,20 @@
+# Flask
+SECRET_KEY = 'supersecret'
+SERVER_NAME = 'localhost:8000'
+SQLALCHEMY_RECORD_QUERIES = True
+DEBUG = True
+
+# Database
+SQLALCHEMY_DATABASE_URI = 'postgresql://username:devpassword@postgres:5432/username'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_RECORD_QUERIES = True
+
+
+# Mail
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'example@gmail.com'
+MAIL_PASSWORD = 'supersecret'
+CC_MAIL_SUBJECT_PREFIX = '[Flask Boilerplate]'
+CC_MAIL_SENDER = 'Flask Boilerplate <flask@example.com>'
