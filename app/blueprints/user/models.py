21b93ca72f9b5c1ffd9061a0c5fd794a2c04ebb4
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-04-06 18:15:39 -0700

    Added login and register routes

--- app/blueprints/user/models.py
+++ app/blueprints/user/models.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from app import db
 
 
@@ -13,12 +16,12 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False,
                          index=True)
     password = db.Column(db.String(128), nullable=False)
-    email_confirmed = db.Column(db.Boolean(), server_default='0')
     session_token = db.Column(db.String(512))
     is_active = db.Column(db.Boolean(), nullable=False, server_default='1')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
+        self.password = User.encrypt_password(kwargs.get('password'))
 
     def __repr__(self):
         return '<User %r>'.format(self.username)
@@ -30,3 +33,35 @@ class User(db.Model):
             'session_token': self.session_token
         }
         return res
+
+    @classmethod
+    def encrypt_password(cls, password):
+        """
+        Hash a plaintext password.
+        :param password: str Plaintext password
+        :return: str Hashed password
+        """
+        return generate_password_hash(password)
+
+    def authenticated(self, password):
+        """
+        Authenticate plaintext password against hashed password saved in
+        the db.
+        :param password: str Plaintext password
+        :return: bool
+        """
+        return check_password_hash(self.password, password)
+
+    def generate_auth_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    # DB Helpers
+    @classmethod
+    def find_by_identity(cls, identity):
+        """
+        Find a user by either their email or username.
+        :param identity: Email or username
+        """
+        return User.query.filter(
+            (User.email == identity) | (User.username == identity)).first()
--- app/blueprints/user/routes.py
+++ app/blueprints/user/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from .models import User
+from flask import Blueprint, jsonify, g, request
+from .models import User, db
 
 user = Blueprint('user', __name__)
 
@@ -12,9 +12,51 @@ def index():
 
 @user.route('/users/register', methods=['POST'])
 def register():
-    return 'Register'
+    username = request.json.get('username')
+    password = request.json.get('password')
+    email = request.json.get('email')
+    if username and password and email:
+        if User.find_by_identity(username) or User.find_by_identity(email):
+            return jsonify({'response':
+                           {'message':
+                            'User with that email or username already exists'}}), 400
+        elif len(password.strip()) < 6:
+            return jsonify({'response':
+                           {'message':
+                            'Password must be at least 6 characters long'}}), 400
+        elif len(username.strip()) < 3:
+            return jsonify({'response':
+                           {'message':
+                            'Username must be at least 3 characters long'}}), 400
+        user = User(username=username.lower(),
+                    email=email.lower(), password=password)
+        session_token = user.generate_auth_token(3600)
+        user.session_token = session_token
+        db.session.add(user)
+        db.session.commit()
+        g.current_user = user
+        response = user.to_json()
+        return jsonify({"response": response}), 200
+    else:
+        return jsonify({'response':
+                       {'message': 'Invalid values provided'}}), 500
 
 
 @user.route('/users/login', methods=['POST'])
 def login():
-    return 'Login'
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username and password:
+        user = User.find_by_identity(username)
+        if user and user.authenticated(password):
+            g.current_user = user
+            session_token = user.generate_auth_token(3600)
+            user.session_token = session_token
+            db.session.commit()
+            response = user.to_json()
+            return jsonify({'response': response}), 200
+        else:
+            return jsonify({'response':
+                           {'message': 'Username or password is wrong'}}), 404
+    return jsonify({'response':
+                   {'message': 'Password and username not provided'}}), 500
